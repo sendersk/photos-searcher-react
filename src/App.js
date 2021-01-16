4d4be7b36b687f4c2cf54648fb609134a643a1c7
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-16 12:03:10 +0100

    Home component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import React from "react";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
