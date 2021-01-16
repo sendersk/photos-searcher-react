27c71734031aaf95ede12fa8084de999ecd9206a
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-16 12:50:12 +0100

    search-container created

--- src/components/SearchPhotos.js
+++ src/components/SearchPhotos.js
@@ -1,9 +1,18 @@
 import React from "react";
+import "./SearchPhotos.css"
 
-function SearchPhotos(){
-    return (
-        <div></div>
-    )
+function SearchPhotos() {
+  return (
+    <div className="search-container">
+      <div className="search-item"></div>
+      <input
+        type="text"
+        name="query"
+        className="search-input"
+        placeholder="Search free high-resolution photos"
+      ></input>
+    </div>
+  );
 }
 
-export default SearchPhotos
+export default SearchPhotos;
