2579ecb36e7906beb6a6a5cd97098198220e6b55
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-16 12:03:31 +0100

    component content created

---
+++ src/components/HeroSection.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./HeroSection.css";
+import backgroundImage from "../images/home-background.jpg";
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <img src={backgroundImage} alt="" className="hero-image" />
+      <div className="hero-content">
+        <h1 className="hero-header">Unsplash</h1>
+        <h2 className="hero-subHeader">
+          The internet's source of freely-usable images.
+          <br />
+          Powered by creators everywhere.
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
